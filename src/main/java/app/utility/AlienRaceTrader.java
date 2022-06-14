57e8d7e52350a1fd3bac0697d5f2ef8ed9243fca
Adrian <arisuwired@gmail.com>
2022-06-14 14:47:23 +0200

    small change

--- src/main/java/app/utility/AlienRaceTrader.java
+++ src/main/java/app/utility/AlienRaceTrader.java
@@ -15,6 +15,7 @@ public class AlienRaceTrader {
         String name1 = alienOneShip.getOwner().getName();
         String name2 = alienTwoSolarSystem.getOwner().getName();
 
+        int oldRel = galaxy.getAlienRelationships().getRelations().get(name1).get(name2).getRelationMeter();
         galaxy.getAlienRelationships().changeRelationship(name1, name2, randomDelta);
 
         int resources = alienOneShip.getResources();
@@ -22,7 +23,7 @@ public class AlienRaceTrader {
         int money = resources * (rel + 120) / 10;
         if (resources != 0)
             System.out.println("\t\tThey traded " + resources + " resources for " + money + " money");
-        System.out.println("\t\tRelationship changed to: " + rel);
+        System.out.println("\t\tRelationship changed from: " + oldRel + " to: " + rel);
 
         alienTwoSolarSystem.addResourcesExtracted(resources);
         alienOneShip.getOwner().addMoney(money);
